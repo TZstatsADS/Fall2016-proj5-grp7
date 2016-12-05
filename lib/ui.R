242d1e4a83d0d8de3d803dc9cd6c3bd64629f518
Shujin Cao (Jackie) <sc3977@columbia.edu>
2016-12-05 02:51:08 -0500

    change

--- lib/ui.R
+++ lib/ui.R
@@ -3,40 +3,26 @@ sidebar <- dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-
 )
 )
 ),
-  sidebarMenu(
-    menuItem("Interactive Map", tabName = "Map", icon = icon("map")),
-    menuItem("EDA", icon = icon("th"), tabName = "EDA",
-             badgeLabel = "new", badgeColor = "green")
-  )
+sidebarMenu(
+  menuItem("Interactive Map", tabName = "Map", icon = icon("map")),
+  menuItem("EDA", icon = icon("th"), tabName = "EDA",badgeLabel = "new", badgeColor = "green"),
+  menuItem("Popularity in Words", icon("th"),tabName="Popularity")
+)
 )
 
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "Map",
             leafletOutput("map2", width = "100%", height = 900),
-            div(class="outer",
-                tags$head(
-                  includeCSS("styles.css")
-                ),
+            div(class="outer",tags$head(includeCSS("styles.css")),
             absolutePanel(id = "controls",class = "panel panel-default", fixed = TRUE,
                           draggable = TRUE, top = 70, left = 300, right = "auto", bottom = "auto",
                           width = 330, height = "auto",
-                          sliderInput("slider2", label = h3("24-hour Range"), min = 0, 
-                                      max = 24, value = c(2, 16), step = 2),
-                          sliderInput("slider1", label = h3("Threshold"), min = 0, 
-                                      max = 300000, value = c(50, 300000))
-            ))
+                          sliderInput("slider2", label = h3("24-hour Range"), min = 0, max = 24, value = c(2, 16), step = 2),
+                          sliderInput("slider1", label = h3("Threshold"), min = 0, max = 300000, value = c(50, 300000))))
     ),
-    
-    tabItem(tabName = "EDA",
-            h2("Widgets tab content")
-            
-    )
-  )
+    tabItem(tabName = "EDA",h2("Widgets tab content")))
 )
 
-
-dashboardPage(skin = "black",dashboardHeader(title = "NYC TAXI"),
-  sidebar,
-  body
+dashboardPage(skin = "black",dashboardHeader(title = "NYC TAXI"),sidebar,body
 )
