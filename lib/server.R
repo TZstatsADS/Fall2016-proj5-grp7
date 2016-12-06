faa525a7ddc95830203c09d6e26b35f8100ea10e
Shujin Cao (Jackie) <sc3977@columbia.edu>
2016-12-06 02:12:11 -0500

    update time

--- lib/server.R
+++ lib/server.R
@@ -3,28 +3,50 @@ library(shiny)
 library(readr)
 library(dplyr)
 library(plotly)
-library(rCharts)
 
 shinyServer(function(input,output){
   
   ride.counts <- read_csv("count_table2.csv")
-  ride.counts.all <- ride.counts %>% 
-    dplyr::group_by(pickup_lon, pickup_lat, dropoff_lon, dropoff_lat) %>%
-    dplyr::summarise(total.count = sum(n))
+  # ride.counts.all <- ride.counts %>% 
+  #   dplyr::group_by(pickup_lon, pickup_lat, dropoff_lon, dropoff_lat, time_interval) %>%
+  #   dplyr::summarise(total.count = sum(n))
   
   minthreshold <- eventReactive(input$button,{
     minthreshold <- input$slider1[1]
   })
-
+  
   maxthreshold <- eventReactive(input$button,{
     maxthreshold <- input$slider1[2]
   })
-
-  ride.counts.filter.threshold <- eventReactive(input$button,{
-    ride.counts.filter.threshold <- ride.counts.all %>%
-      filter(total.count >= minthreshold() & total.count < maxthreshold())
+  
+  minhour <- eventReactive(input$button,{
+    minhour <- input$slider2[1]
   })
-
+  maxhour <- eventReactive(input$button,{
+    maxhour <- input$slider2[2]
+  })
+  
+  ride.counts.filter.threshold <- eventReactive(input$button,{
+    ride <- ride.counts %>%
+      filter(n >= minthreshold() & n < maxthreshold()) %>% 
+      filter(time_interval >= minhour() & time_interval <= maxhour())
+    # ride.counts.filter.threshold <- ride.counts.filter.threshold[,-which(names(ride.counts.filter.threshold %in% c("time_interval")))]
+    ride.counts.filter.threshold <- ride[,-c(1:3)]
+  })
+  
+  # ride.counts.filter.threshold <- eventReactive(input$button,{
+  #   ride.counts.filter.threshold <- ride.counts.all %>%
+  #     filter(total.count >= minthreshold() & total.count < maxthreshold()) %>%
+  #     filter(time_interval >= minhour() & time_interval >= maxhour())
+  # })
+  
+  # ride.counts.filter.threshold <- ride.counts.filter.threshold %>% 
+  #   dplyr::group_by(pickup_lon, pickup_lat, dropoff_lon, dropoff_lat)
+  # ride.counts.filter.threshold <- eventReactive(input$button,{
+  #   ride.counts.filter.threshold <- ride.counts.all %>% 
+  # 
+  # })
+  
   # ride.counts.filter.threshold <- reactiveValues()
   # ride.counts.filter.threshold$data <- as.matrix(ride.counts.all) 
   # observe({
@@ -77,66 +99,66 @@ shinyServer(function(input,output){
   }
   
   
+  
+  paint.arrows <- function(map, arrow.data){
     
-    paint.arrows <- function(map, arrow.data){
-      
-      arrow.data <- as.matrix(arrow.data)
-      
-      for(i in 1:nrow(arrow.data)){
-        
-        # Get Data from row
-        lng.x = c(arrow.data[i,1], arrow.data[i,3])
-        lat.x = c(arrow.data[i,2], arrow.data[i,4])
-        weight.x = arrow.data[i,5]
-        maxweight = max(arrow.data[,5])
-        line.weight = as.numeric(weight.x / maxweight * 20)  
-        
-        # Paint line
-        
-        map <- addPolylines(map, lng = lng.x, lat = lat.x, weight = line.weight)
-        
-        
-        ## Paint Arrow Heads
-        
-        # Get Data of arrow head
-        arrow.head <- get.arrowhead(arrow.data[i,1:4])
-        # Check if we had errors (possible because origin and destination are the same)
-        error.values <- sum(is.na(arrow.head))
-        
-        # If everything OK, we calculate and paint
-        if(error.values == 0){
-          lng.arrow.1 <- c(arrow.head[1,1], arrow.head[1,3])
-          lat.arrow.1 <- c(arrow.head[1,2], arrow.head[1,4])
-          lng.arrow.2 <- c(arrow.head[2,1], arrow.head[2,3])
-          lat.arrow.2 <- c(arrow.head[2,2], arrow.head[2,4])
-          
-          # Paint Arrow Head
-          map <- addPolylines(map, lng = lng.arrow.1, lat = lat.arrow.1, weight = line.weight)    
-          map <- addPolylines(map, lng = lng.arrow.2, lat = lat.arrow.2, weight = line.weight)        
-        }
-      }  
-      
-      return(map)
-    }
+    arrow.data <- as.matrix(arrow.data)
     
-    n<-leaflet()%>%
-      addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
-               attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-      )%>%
-      setView(lng = -73.97, lat = 40.75, zoom = 13)
+    for(i in 1:nrow(arrow.data)){
+      
+      # Get Data from row
+      lng.x = c(arrow.data[i,1], arrow.data[i,3])
+      lat.x = c(arrow.data[i,2], arrow.data[i,4])
+      weight.x = arrow.data[i,5]
+      maxweight = max(arrow.data[,5])
+      line.weight = as.numeric(weight.x / maxweight * 20)  
+      
+      # Paint line
+      
+      map <- addPolylines(map, lng = lng.x, lat = lat.x, weight = line.weight)
+      
+      
+      ## Paint Arrow Heads
+      
+      # Get Data of arrow head
+      arrow.head <- get.arrowhead(arrow.data[i,1:4])
+      # Check if we had errors (possible because origin and destination are the same)
+      error.values <- sum(is.na(arrow.head))
+      
+      # If everything OK, we calculate and paint
+      if(error.values == 0){
+        lng.arrow.1 <- c(arrow.head[1,1], arrow.head[1,3])
+        lat.arrow.1 <- c(arrow.head[1,2], arrow.head[1,4])
+        lng.arrow.2 <- c(arrow.head[2,1], arrow.head[2,3])
+        lat.arrow.2 <- c(arrow.head[2,2], arrow.head[2,4])
+        
+        # Paint Arrow Head
+        map <- addPolylines(map, lng = lng.arrow.1, lat = lat.arrow.1, weight = line.weight)    
+        map <- addPolylines(map, lng = lng.arrow.2, lat = lat.arrow.2, weight = line.weight)        
+      }
+    }  
     
-    # n=paint.arrows(n, ride.counts.filter.threshold)
-    output$map2 <- renderLeaflet({
-      # n <- Leaflet$new()
-      # n$setView(c(40.75,-73.97),zoom = 13)
-      # n$tileLayer(provider='Stamen.TonerLite') 
-      #   addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
-      #            attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-      #   )%>%
-      #   setView(lng = -73.97, lat = 40.75, zoom = 13)
-      m <- ride.counts.filter.threshold()
-      map <- paint.arrows(n, m)
-      map
+    return(map)
+  }
+  
+  n<-leaflet()%>%
+    addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
+             attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+    )%>%
+    setView(lng = -73.97, lat = 40.75, zoom = 13)
+  
+  # n=paint.arrows(n, ride.counts.filter.threshold)
+  output$map2 <- renderLeaflet({
+    # n <- Leaflet$new()
+    # n$setView(c(40.75,-73.97),zoom = 13)
+    # n$tileLayer(provider='Stamen.TonerLite') 
+    #   addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
+    #            attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+    #   )%>%
+    #   setView(lng = -73.97, lat = 40.75, zoom = 13)
+    m <- ride.counts.filter.threshold()
+    map <- paint.arrows(n, m)
+    map
     # output$map2 <- renderLeaflet({
     # leaflet()%>%
     #   addTiles(urlTemplate = "https://api.mapbox.com/styles/v1/jackiecao/ciu0jcgy800ah2ipgpsw5usmi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja2llY2FvIiwiYSI6ImNpdTBqYXhmcjAxZ24ycGp3ZWZ1bTJoZ3QifQ.VytIrn5ZxVjtZjM15JPA9Q",
--- lib/ui.R
+++ lib/ui.R
@@ -1,8 +1,8 @@
 library(shinydashboard)
 library(leaflet)
-library(rCharts)
+
 sidebar <- dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-family: "Britannic Bold",Britannic Bold, 
-                                                           "Britannic Bold", serif;font-weight: bold;font-size: 22px;}'
+                                                      "Britannic Bold", serif;font-weight: bold;font-size: 22px;}'
 )
 )
 ),
@@ -19,14 +19,14 @@ body <- dashboardBody(
             leafletOutput("map2", width = "100%", height = 900),
             # plotOutput("map2"),
             div(class="outer",tags$head(includeCSS("styles.css")),
-            absolutePanel(id = "controls",class = "panel panel-default", fixed = TRUE,
-                          draggable = TRUE, top = 70, left = 300, right = "auto", bottom = "auto",
-                          width = 330, height = "auto",
-                          sliderInput("slider2", label = h3("24-hour Range"), min = 0, max = 24, value = c(2, 16), step = 2),
-                          sliderInput("slider1", label = h3("Threshold"), min = 5000, max = 10000, value = c(50, 300000), step = 1000),
-                          actionButton("button", 'UPDATE', icon = shiny::icon('calendar'), style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
-                          ))
-                          
+                absolutePanel(id = "controls",class = "panel panel-default", fixed = TRUE,
+                              draggable = TRUE, top = 70, left = 300, right = "auto", bottom = "auto",
+                              width = 330, height = "auto",
+                              sliderInput("slider2", label = h3("24-hour Range"), min = 0, max = 24, value = c(2, 16), step = 2),
+                              sliderInput("slider1", label = h3("Threshold"), min = 1000, max = 4000, value = c(500, 4500), step = 100),
+                              actionButton("button", 'UPDATE', icon = shiny::icon('calendar'), style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+                ))
+            
     ),
     tabItem(tabName = "EDA",h2("Widgets tab content")))
 )
