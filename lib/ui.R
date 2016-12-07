0bb394f8049f161f7db61656ae0be4f11672427b
Shujin Cao (Jackie) <sc3977@columbia.edu>
2016-12-07 09:39:01 -0500

    change

--- lib/ui.R
+++ lib/ui.R
@@ -1,5 +1,7 @@
 library(shinydashboard)
 library(leaflet)
+library(plotly)
+
 topChoices <- c(1,5,10,20,50,100,500,1000)
 sidebar <- dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-family: "Britannic Bold",Britannic Bold, 
                                                       "Britannic Bold", serif;font-weight: bold;font-size: 22px;}'
@@ -9,7 +11,7 @@ sidebar <- dashboardSidebar(tags$head(tags$style(HTML('.main-header .logo {font-
 sidebarMenu(
   menuItem("Interactive Map", tabName = "Map", icon = icon("map")),
   menuItem("EDA", icon = icon("th"), tabName = "EDA",badgeLabel = "new", badgeColor = "green"),
-  menuItem("Popularity in Words", icon("th"),tabName="Popularity")
+  menuItem("Connections", icon = icon("cog"),tabName="Connections")
 )
 )
 
@@ -22,8 +24,8 @@ body <- dashboardBody(
             # plotOutput("map2"),
             div(class="outer",tags$head(includeCSS("styles.css")),
                 absolutePanel(id = "controls",class = "panel panel-default", fixed = TRUE,
-                              draggable = TRUE, top = 70, left = 300, right = "auto", bottom = "auto",
-                              width = 330, height = "auto",
+                              draggable = FALSE, top = "auto", left = "auto", right = 15, bottom = 0,
+                              width = 250, height = 400,
                               sliderInput("slider2", label = h3("24-hour Range"), min = 0, max = 22, value = c(4, 6), step = 2),
                               # sliderInput("slider1", label = h3("Threshold"), min = 1000, max = 4000, value = c(500, 4500), step = 100),
 
@@ -39,8 +41,10 @@ body <- dashboardBody(
     tabItem(tabName = "EDA",h2("Widgets tab content"),
             selectInput(inputId = "table_month",label = "PICK YEAR-MONTH:",
                         choices = c("2015-07","2015-08","2015-09","2015-10","2015-11","2015-12","2016-01","2016-02","2016-03","2016-04","2016-05","2016-06")),
-            plotlyOutput("taxi_time_table_plot")))
+            plotlyOutput("taxi_time_table_plot"))
+    # tabItem(tabName = "Connections", column(4,includeHTML("include.md"),tags$a(href="http://rpubs.com/zuozuo/233483", "Click Here!")
+    # ))
+    )
 )
-
 dashboardPage(skin = "black",dashboardHeader(title = "NYC TAXI"),sidebar,body
 )
